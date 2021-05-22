9eadfc4ba21cc825f804db60ae2a73d16a1d1658
AutomotiveEngTolgaCOSKUN <82567648+AutomotiveEngTolgaCOSKUN@users.noreply.github.com>
2021-05-22 20:51:26 +0300

    thirdHomeworkOfSixthDay

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobPosition.java
@@ -0,0 +1,33 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "add_general_job_title_names")
+@Data
+public class JobPosition {
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "position_name")
+	private String positionName;
+
+	public JobPosition() {
+
+	}
+
+	public JobPosition(int id, String positionName) {
+		super();
+		this.id = id;
+		this.positionName = positionName;
+	}
+
+}
