011c42689922d5dd5e855f8eb3b80ab766fd7643
AutomotiveEngTolgaCOSKUN <82567648+AutomotiveEngTolgaCOSKUN@users.noreply.github.com>
2021-05-26 09:35:51 +0300

    firstHomeworkOfSeventhDay

--- hrms/src/main/java/kodlamaio/hrms/business/concretes/JobPositionManager.java
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/JobPositionManager.java
@@ -5,6 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.JobPositionService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
+import kodlamaio.hrms.core.utilities.results.SuccessResult;
 import kodlamaio.hrms.dataAccess.abstracts.JobPositionDao;
 import kodlamaio.hrms.entities.concretes.JobPosition;
 
@@ -19,9 +23,15 @@ public class JobPositionManager implements JobPositionService {
 	}
 
 	@Override
-	public List<JobPosition> getAll() {
+	public DataResult<List<JobPosition>> getAll() {
 		// TODO Auto-generated method stub
-		return this.jobPositionDao.findAll();
+		return new SuccessDataResult<List<JobPosition>>(this.jobPositionDao.findAll(), "Listed Data");
+	}
+
+	@Override
+	public Result add(JobPosition jobPosition) {
+		this.jobPositionDao.save(jobPosition);
+		return new SuccessResult("Added Job Position");
 	}
 
 }
